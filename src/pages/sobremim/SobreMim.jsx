e111eaf4c5fa2f34f6a6222202ab20c35c415332
Caio Azeredo <caiodoss15@gmail.com>
2024-09-25 15:18:54 -0300

    atualizacao sobremim

--- src/pages/sobremim/SobreMim.jsx
+++ src/pages/sobremim/SobreMim.jsx
@@ -8,10 +8,13 @@ function SobreMim() {
                 Olá! Me chamo Caio Azeredo, tenho 20 anos e sou graduando em Sistemas de Computação pela Universidade Federal Fluminense (UFF). Ainda estou no início da graduação, mas já muito motivado e ansioso para progredir na área.
             </p> <br />
             <p>
-                Sou amante da tecnologia e suas aplicações, e tenho maior interesse especialmente nas áreas de Desenvolvimento Front-End e Análise de Dados. Ainda não possuo experiência no mercado de trabalho, porém construí este portfólio com alguns projetos, que comprovam as habilidades que mencionei. Portanto, meu principal objetivo no momento é encontrar uma oportunidade para aprender e/ou aprimorar meus conhecimentos nas áreas que mencionei, como estagiário ou trainee.
+                Sou amante da tecnologia e suas aplicações, e tenho maior interesse especialmente nas áreas de Desenvolvimento Front-End e Análise de Dados. Embora ainda não tenha experiência formal no mercado de trabalho, construí este portfólio com alguns projetos que comprovam as habilidades que mencionei.
             </p> <br />
             <p>
-                Na área de desenvolvimento, tenho habilidades em versionamento de código e hospedagem de projetos com Git e GitHub e nas tecnologias HTML, CSS, JavaScript e React.js. Partindo para a área de dados, tenho conhecimentos em análises básicas de dados e construção de dashboards simples com Excel e Power BI.
+                Na área de desenvolvimento, tenho conhecimentos em versionamento de código e hospedagem de projetos utilizando Git e GitHub, além das tecnologias HTML, CSS, JavaScript e React.js. Na área de dados, possuo conhecimentos em bancos de dados, com foco em SQL e MySQL, e habilidades básicas em análise de dados e construção de dashboards simples com Excel e Power BI.
+            </p> <br />
+            <p>
+                Meu objetivo principal é encontrar uma oportunidade como estagiário ou trainee, onde eu possa aprender e aprimorar minhas habilidades nas áreas mencionadas.
             </p>
         </div>
     )
