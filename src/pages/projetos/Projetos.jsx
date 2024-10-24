ee00c7759e77ab1807f479b96d2187b57c2cd3b8
Caio Azeredo <caiodoss15@gmail.com>
2024-10-23 21:24:06 -0300

    addimg

--- src/pages/projetos/Projetos.jsx
+++ src/pages/projetos/Projetos.jsx
@@ -4,11 +4,38 @@ import styles from './Projetos.module.css'
 import imgProjeto1 from '../../assets/img-projeto1.png'
 import imgProjeto2 from '../../assets/img-projeto2.png'
 import imgProjeto3 from '../../assets/img-projeto3.png'
+import imgProjeto4 from '../../assets/img-projeto4.png'
 
 function Projetos() {
     return (
         <div className={styles.divprojetos}>
             <h1>Meus projetos</h1>
+
+            <div className={styles.projeto}>
+                <div className={styles.imgprojeto}>
+                    <img src={imgProjeto4} alt="Imagem projeto" />
+                </div>
+
+                <div className={styles.infoprojeto}>
+                    <h2>Projeto Análise de Dados de Vendas</h2>
+                    <p>Este projeto de análise de dados foi desenvolvido para uma empresa, utilizando Python (pandas) para análise e Power BI para visualização dos dados. A análise abordou os seguintes desafios: relação entre gênero e tipo de produto comprado, distribuição de clientes por faixa etária, relação entre a classificação do produto e a quantidade de vendas, compras por mês, métodos de pagamento mais utilizados e cancelamentos por tipo de produto. <br /> <br />
+Com base nessas análises, foram gerados indicadores sobre as vendas, baseados no comportamento do cliente, o que pode oferecer insights valiosos para a gestão das vendas da empresa. Por fim, foi criado um dashboard interativo no Power BI para facilitar a visualização das métricas e das análises. <br /> <br />
+
+Obs.: a base de dados utilizada foi importada do Kaggle.</p>
+                    <p><span>Tecnologias utilizadas: Python (pandas) e Power BI</span></p>
+                </div>
+
+                <div className={styles.botaoprojeto}>
+                <button>
+                        <a className={styles.linkprojeto} href="https://app.powerbi.com/view?r=eyJrIjoiNmIwZTNjZDUtN2UyZC00YTJkLTk3YjktNTAxNDZlY2EwMmIwIiwidCI6ImIyZTE2Mjk3LTJlZDYtNDFiOC1iODIyLWE5NTRlOTViZDJmMCIsImMiOjR9" target='_blank'><FaEye /> Visualizar dashboard</a>
+                    </button>
+                    <button>
+                        <a className={styles.linkprojeto} href="https://github.com/caiomangabeira/projeto-analise-comp-cliente" target='_blank'>
+                            <FaGithub /> Código da análise
+                        </a>
+                    </button>
+                </div>
+            </div>
 
             <div className={styles.projeto}>
                 <div className={styles.imgprojeto}>
@@ -64,7 +91,7 @@ function Projetos() {
                 <div className={styles.infoprojeto}>
                 <h2>Projeto Previsão do Tempo</h2>
                     <p>Esta é uma pagina que exibe a previsão do tempo de uma cidade por meio de requisições. Primeiro, ao usuário digitar o nome de uma cidade e enviar, uma requisição é feita para obter as coordenadas dessa cidade, através de uma API. Depois, utilizando as coordenadas retornadas por essa API, outra requisição é feita, retornando dados meteorológicos da cidade e exibindo para o usuário. Obs.: este projeto não é responsivo para celulares e tablets. É voltado para exibição em desktops e notebooks.</p>
-                    <p><span>Tecnologias utilizadas: HTML, CSS e JavaScript.</span></p>
+                    <p><span>Tecnologias utilizadas: HTML, CSS, JavaScript e API.</span></p>
                 </div>
 
                 <div className={styles.botaoprojeto}>
